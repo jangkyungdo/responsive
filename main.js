7582ca89906c6eaa43686c78600b8fe408502c47
jangkyungdo <rudeh0926@naver.com>
2020-06-20 23:19:43 +0900

    directory 정리

--- main.js
+++ main.js
@@ -16,7 +16,8 @@ const arrow = document.querySelector(".arrowup--btn");
 const nav = document.querySelector("#navbar");
 const navHeight = nav.getBoundingClientRect().height;
 
-console.log(navHeight);
+let variable = false;
+
 document.addEventListener("DOMContentLoaded", () => {
   // 클릭 이벤트 코드
   toggleBtn.addEventListener("click", () => {
@@ -39,28 +40,21 @@ document.addEventListener("DOMContentLoaded", () => {
     window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
   });
 
+  let variable = false;
+
   // 슬라이드  코드
   if (slidebox) {
-    // slide가 있을때 동작
-    for (let i = 0; i < slideItem.length; i++) {
-      if (slideHeight < slideItem[i].offsetHeight) {
-        slideHeight = slideItem[i].offsetHeight;
-      }
-    }
-
-    // slideContainer.style.height = slideHeight + "px";
-    // slidebox.style.height = slideHeight + "px";
-    // slideList.style.height = slideHeight + "px";
-
-    // slideItem(li) left값을 준다. li가로정렬
-    // for (let i = 0; i < slideItem.length; i++) {
-    //   slideItem[i].style.left = `${100 * i}%`;
-    // }
     slideItem.forEach((item, index) => {
       item.style.left = `${100 * index}%`;
     });
 
     next.addEventListener("click", () => {
+      if (variable) {
+        console.log("variable = true");
+        return;
+      }
+      variable = true;
+
       if (current === slideItem.length - 3) {
         goToSlide(3);
         slideList.style.transition = `300ms`;
@@ -72,9 +66,17 @@ document.addEventListener("DOMContentLoaded", () => {
         slideList.style.transition = `300ms`;
         goToSlide(current + 1);
       }
+      console.log("variable = false");
+      variable = false;
     });
 
     prev.addEventListener("click", () => {
+      if (variable) {
+        console.log("variable = true");
+        return;
+      }
+      variable = true;
+
       if (current === 0) {
         goToSlide(-1);
         slideList.style.transition = `300ms`;
@@ -86,6 +88,8 @@ document.addEventListener("DOMContentLoaded", () => {
         slideList.style.transition = `300ms`;
         goToSlide(current - 1);
       }
+      console.log("variable = false");
+      variable = false;
     });
   }
 
@@ -94,43 +98,3 @@ document.addEventListener("DOMContentLoaded", () => {
     current = index;
   }
 });
-
-// const slide = document.querySelector(".slide__list");
-// const slideItem = document.querySelector(".slide__item");
-
-// const slideWidth = slideItem.getBoundingClientRect().width;
-
-// document.addEventListener("DOMContentLoaded", function () {
-//   let current = 0;
-//   if (slide) {
-//     setInterval(function () {
-//       /* 일정한 시간 간격으로 작업을 수행하는 함수 */
-//       slide.style.transition = "1s";
-//       slide.style.transform =
-//         "translateX(-" + slideWidth * (current + 1) + "px")";
-
-//       current++;
-
-//       if (current === 2) {
-//         current = -1;
-//       }
-//     }, 3000);
-//   }
-//   // DOMContentLoaded 이벤트는 초기 HTML 문서를 완전히 불러오고 분석했을 때 발생한다.
-// });
-
-// const arrowUpBtn = document.querySelector(".arrowup--btn");
-// const header = document.querySelector("#main");
-// const headerHeight = header.getBoundingClientRect().height;
-
-// document.addEventListener("scroll", () => {
-//   if (window.scrollY > headerHeight) {
-//     arrowUpBtn.classList.add("visible");
-//   } else {
-//     arrowUpBtn.classList.remove("visible");
-//   }
-// });
-
-// arrowUpBtn.addEventListener("click", () => {
-//   header.scrollIntoView({ behavior: "smooth" });
-// });
