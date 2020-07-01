636e7c8db25ff76e3305426a80f77ea375dc98eb
jangkyungdo <rudeh0926@naver.com>
2020-07-01 12:25:15 +0900

    slide update test

--- main.js
+++ main.js
@@ -40,58 +40,102 @@ document.addEventListener("DOMContentLoaded", () => {
     window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
   });
 
+  // 데이터가 중복적으로 들어오는 것을 방지하기 위한 변수
   let variable = false;
 
+  prev.addEventListener("click", () => {
+    if (variable) {
+      console.log("variable = true");
+      return;
+    }
+    variable = true;
+
+    slideList.style.transitionProperty = "none"; //transtion 충돌로 인한 오류?를 잡기위한 코드, 바로 실행되야하는 부분이 transtion값이 적용이 되여서
+    slideList.style.left = "-100%";
+
+    let lastChild = slideList.lastElementChild;
+    let cloneLastChild = lastChild.cloneNode(true);
+    slideList.prepend(cloneLastChild); //slideList에 복제한 첫번째 자식을 뒤에 다 넣음
+    lastChild.remove(); //기존 첫번째 자식은 없앰
+
+    setTimeout(() => {
+      slideList.style.transition = "all 800ms";
+      slideList.style.left = "0%"; //left 0 으로 만들어줌으로 더이상 왼쪽으로 가지 못하게 막아줌
+      variable = false;
+    }, 100);
+  });
+
+  next.addEventListener("click", () => {
+    if (variable) {
+      console.log("variable = true");
+      return;
+    }
+    variable = true;
+
+    slideList.style.transition = "all 800ms";
+    slideList.style.left = "-100%";
+
+    setTimeout(() => {
+      slideList.style.transitionProperty = "none"; //transtion 충돌로 인한 오류?를 잡기위한 코드, 바로 실행되야하는 부분이 transtion값이 적용이 되여서
+      let firstChild = slideList.firstElementChild;
+      let cloneFirstChild = firstChild.cloneNode(true);
+      slideList.append(cloneFirstChild); //sliderBox에 복제한 첫번째 자식을 뒤에 다 넣음
+      firstChild.remove(); //기존 첫번째 자식은 없앰
+      slideList.style.left = "0%"; //left 0 으로 만들어줌으로 더이상 왼쪽으로 가지 못하게 막아줌
+      variable = false;
+    }, 800);
+  });
+
   // 슬라이드  코드
-  if (slidebox) {
-    slideItem.forEach((item, index) => {
-      item.style.left = `${100 * index}%`;
-    });
+  // if (slidebox) {
+  //   slideItem.forEach((item, index) => {
+  //     item.style.left = `${100 * index}%`;
+  //   });
 
-    next.addEventListener("click", () => {
-      if (variable) {
-        console.log("variable = true");
-        return;
-      }
-      variable = true;
+  //   next.addEventListener("click", () => {
+  //     if (variable) {
+  //       console.log("variable = true");
+  //       return;
+  //     }
+  //     variable = true;
 
-      if (current === slideItem.length - 3) {
-        goToSlide(3);
-        slideList.style.transition = `300ms`;
-        setTimeout(() => {
-          slideList.style.transition = `0s`;
-          goToSlide(0);
-        }, 300);
-      } else {
-        slideList.style.transition = `300ms`;
-        goToSlide(current + 1);
-      }
-      console.log("variable = false");
-      variable = false;
-    });
+  //     if (current === slideItem.length - 3) {
+  //       goToSlide(3);
+  //       slideList.style.transition = `300ms`;
+  //       setTimeout(() => {
+  //         slideList.style.transition = `0s`;
+  //         goToSlide(0);
+  //       }, 300);
+  //     } else {
+  //       slideList.style.transition = `300ms`;
+  //       goToSlide(current + 1);
+  //     }
+  //     console.log("variable = false");
+  //     variable = false;
+  //   });
 
-    prev.addEventListener("click", () => {
-      if (variable) {
-        console.log("variable = true");
-        return;
-      }
-      variable = true;
+  //   prev.addEventListener("click", () => {
+  //     if (variable) {
+  //       console.log("variable = true");
+  //       return;
+  //     }
+  //     variable = true;
 
-      if (current === 0) {
-        goToSlide(-1);
-        slideList.style.transition = `300ms`;
-        setTimeout(() => {
-          slideList.style.transition = `0s`;
-          goToSlide(2);
-        }, 300);
-      } else {
-        slideList.style.transition = `300ms`;
-        goToSlide(current - 1);
-      }
-      console.log("variable = false");
-      variable = false;
-    });
-  }
+  //     if (current === 0) {
+  //       goToSlide(-1);
+  //       slideList.style.transition = `300ms`;
+  //       setTimeout(() => {
+  //         slideList.style.transition = `0s`;
+  //         goToSlide(2);
+  //       }, 300);
+  //     } else {
+  //       slideList.style.transition = `300ms`;
+  //       goToSlide(current - 1);
+  //     }
+  //     console.log("variable = false");
+  //     variable = false;
+  //   });
+  // }
 
   function goToSlide(index) {
     slideList.style.left = `${-100 * index}%`;
